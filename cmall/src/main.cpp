30085f424ca567aea0f344a7de47efbe0cd3a67b
microcai <microcaicai@gmail.com>
2022-02-25 01:06:56 +0800

    Update main.cpp

--- cmall/src/main.cpp
+++ cmall/src/main.cpp
@@ -143,6 +143,15 @@ std::string version_info()
 	return oss.str();
 }
 
+static boost::asio::awaitable<std::string> get_default_cache_dir()
+{
+#ifndef WIN32
+	co_return "/var/lib/cmall";
+#else
+	co_return ".";
+#endif
+}
+
 boost::asio::awaitable<int> co_main(int argc, char** argv, io_context_pool& ios)
 {
 	platform_init();
@@ -169,7 +178,7 @@ boost::asio::awaitable<int> co_main(int argc, char** argv, io_context_pool& ios)
 		("upstream", po::value<std::vector<std::string>>(&upstreams)->multitoken(), "Upstreams.")
 		("wsorigin", po::value<std::string>(&wsorigin)->default_value(""), "Websocket allowed origin.")
 		("ws", po::value<std::vector<std::string>>(&ws_listens)->multitoken(), "For websocket server listen.")
-		("session_cache", po::value<std::string>(&session_cache)->default_value("/var/lib/cmall"), "the dir for session cache")
+		("session_cache", po::value<std::string>(&session_cache)->default_value(co_await get_default_cache_dir()), "the dir for session cache")
 
 		("db_name", po::value<std::string>(&db_name)->default_value("cmall"), "Database name.")
 		("db_host", po::value<std::string>(&db_host)->default_value(""), "Database host.")
