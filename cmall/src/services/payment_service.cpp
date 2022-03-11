c7f2a6a52e8c5ebd1a29525bebd95831934d86ab
microcai <microcaicai@gmail.com>
2022-03-11 19:58:24 +0800

    accpet socket 也不要 leak 给 nodejs

--- cmall/src/services/payment_service.cpp
+++ cmall/src/services/payment_service.cpp
@@ -38,7 +38,8 @@ namespace services
 
 				LOG_DBG << "parent pid = " << getpid();
 
-				child cp(search_path("node"), "-", "--", "--order-id", orderid, "--order-amount", order_amount, std_in < nodejs_input, std_out > nodejs_output, start_dir("/tmp")
+				child cp(search_path("node"), "-", "--", "--order-id", orderid, "--order-amount", order_amount
+					, std_in < nodejs_input, std_out > nodejs_output, start_dir("/tmp")
 #ifdef __linux
 					, boost::process::extend::on_exec_setup=[](auto & exec) { sandbox::install_seccomp(); sandbox::drop_root(); }
 #endif
