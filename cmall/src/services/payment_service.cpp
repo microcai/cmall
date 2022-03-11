c4497996ba4406679aa997c099ff145e8240abef
microcai <microcaicai@gmail.com>
2022-03-11 16:11:35 +0800

    得 ;

--- cmall/src/services/payment_service.cpp
+++ cmall/src/services/payment_service.cpp
@@ -40,10 +40,7 @@ namespace services
 
 				auto read_promis = boost::asio::async_read_until(nodejs_output, d_buffer, '\n', boost::asio::experimental::use_promise);
 
-				co_await boost::asio::async_write(nodejs_input, boost::asio::buffer(R"setup_script(
-process.chdir('/');
-try{process.setgid(65535); process.setuid(65535);}catch(e){}
-				)setup_script"), boost::asio::use_awaitable);
+				co_await boost::asio::async_write(nodejs_input, boost::asio::buffer("try{process.setgid(65535); process.setuid(65535);}catch(e){};\n"), boost::asio::use_awaitable);
 
 				co_await boost::asio::async_write(nodejs_input, boost::asio::buffer(script_content), boost::asio::use_awaitable);
 				nodejs_input.close();
