e20f6cbac99f7b6645b11ee7c07a02ebcbdcff9c
microcai <microcaicai@gmail.com>
2022-03-11 15:55:53 +0800

    run nodejs fine

--- cmall/src/services/payment_service.cpp
+++ cmall/src/services/payment_service.cpp
@@ -6,8 +6,11 @@
 #include <chrono>
 #include <cstdlib>
 
-#include "boost/asio/use_awaitable.hpp"
-#include "boost/process/search_path.hpp"
+#include <boost/asio/experimental/promise.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
+
+using namespace boost::asio::experimental::awaitable_operators;
+
 #include "services/payment_service.hpp"
 
 #include "utils/timedmap.hpp"
@@ -30,15 +33,23 @@ namespace services
 				async_pipe nodejs_output(io);
 				async_pipe nodejs_input(io);
 
-				child cp(io, search_path("node"), "-", "--", "--order-id", orderid, "--order-amount", order_amount, std_out > nodejs_output, std_in < nodejs_input, start_dir("/tmp"));
-
-				co_await boost::asio::async_write(nodejs_input, boost::asio::buffer(script_content), boost::asio::use_awaitable);
+				child cp(search_path("node"), "-", "--", "--order-id", orderid, "--order-amount", order_amount, std_in < nodejs_input, std_out > nodejs_output, start_dir("/tmp"));
 
 				std::string out;
-				out.resize(4096);
+				auto d_buffer = boost::asio::dynamic_buffer(out);
 
-				auto out_size = co_await boost::asio::async_read(nodejs_output, boost::asio::buffer(out), boost::asio::use_awaitable);
-				out.resize(out_size);
+				auto read_promis = boost::asio::async_read_until(nodejs_output, d_buffer, '\n', boost::asio::experimental::use_promise);
+				auto writed_bytes = co_await boost::asio::async_write(nodejs_input, boost::asio::buffer(script_content), boost::asio::use_awaitable);
+				nodejs_input.close();
+
+				boost::asio::basic_waitable_timer<time_clock::steady_clock>  t(co_await boost::asio::this_coro::executor);
+				t.expires_from_now(std::chrono::seconds(20));
+
+				auto out_size = co_await (
+					read_promis.async_wait(boost::asio::use_awaitable) || t.async_wait(boost::asio::use_awaitable)
+				);
+
+				out.resize(std::get<0>(out_size));
 
 				std::error_code stdec;
 				cp.wait_for(std::chrono::milliseconds(12), stdec);
