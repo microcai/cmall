7860250cdf19bf3458185bb2e498f2e0a6c6b293
kaishek <kaishek@np.org>
2022-04-13 12:56:10 +0800

    客服消息method

--- cmall/include/cmall/cmall.hpp
+++ cmall/include/cmall/cmall.hpp
@@ -187,6 +187,13 @@ namespace cmall {
 		admin_reenable_merchants,
 		admin_sudo,
 		admin_sudo_cancel,
+
+		// 发客服消息.
+		customer_service_send_message,
+		// 登录后订阅发给自己的消息.
+		customer_service_subscribe_message,
+		// 拉取历史客服聊天记录.
+		customer_service_message_history,
 	};
 
 	class cmall_service
