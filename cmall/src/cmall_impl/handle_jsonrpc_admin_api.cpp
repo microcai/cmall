c432f2be684b9accdee8da6089a3f9308d2d3422
microcai <microcaicai@gmail.com>
2022-07-30 23:50:03 +0800

    admin_list_index_goods

--- cmall/src/cmall_impl/handle_jsonrpc_admin_api.cpp
+++ cmall/src/cmall_impl/handle_jsonrpc_admin_api.cpp
@@ -279,6 +279,34 @@ awaitable<boost::json::object> cmall::cmall_service::handle_jsonrpc_admin_api(cl
         }
         case req_method::admin_list_index_goods:
         {
+			std::vector<cmall_merchant> merchants;
+			co_await m_database.async_load_all<cmall_merchant>(merchants);
+
+			std::map<std::uint64_t, cmall_merchant> cache;
+
+            for (const auto& m : merchants)
+            {
+                cache.insert(std::make_pair(m.uid_, m));
+            }
+
+            std::vector<cmall_index_page_goods> ret;
+
+            odb::query<cmall_index_page_goods> q = " 1=1 order by " + odb::query<cmall_index_page_goods>::order + " asc";
+
+            co_await m_database.async_load<cmall_index_page_goods>(q, ret);
+
+            std::vector<services::product> all_products;
+
+            for (auto gref : ret)
+            {
+                try {
+                    services::product hint_product = co_await get_merchant_git_repo(gref.merchant_id)->get_product(gref.goods, cache[gref.merchant_id].name_, this_client.ws_client->baseurl_);
+                    all_products.push_back(hint_product);
+                }catch(std::exception&)
+                {}
+            }
+
+            reply_message["result"] = boost::json::value_from(all_products);
 
         }break;
         case req_method::admin_add_index_goods:
