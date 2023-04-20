b87f64ed8de50ef457b26ab7974edf86d44f950a
microcai <microcaicai@gmail.com>
2023-04-20 19:30:40 +0800

    httpc add support for gzip,deflate

--- cmall/include/utils/httpc.hpp
+++ cmall/include/utils/httpc.hpp
@@ -19,6 +19,11 @@
 #include <optional>
 #include <iostream>
 
+#if __has_include("zlib.h")
+#include "zlib.h"
+#define HAS_ZLIB 1
+#endif
+
 using boost::asio::awaitable;
 
 namespace httpc
@@ -44,6 +49,56 @@ namespace httpc
 
 	namespace detail
 	{
+		#ifdef HAS_ZLIB
+		namespace gzip
+		{
+			inline std::string gunzip(const char* gzip_data, size_t  gzip_data_len)
+			{
+				std::string deflated_data;
+				// // 原始gzip格式数据
+				// char gzip_data[] = {31, 139, 8, 0, 0, 0, 0, 0, 0, 0, 11, 99, 96, 28, 73, 47, 202, 47, 202, 47, 42, 214, 4, 0, 0, 0};
+				// // 原始数据长度
+				// size_t gzip_data_len = sizeof(gzip_data) / sizeof(char);
+
+				// 创建zlib解压器
+				z_stream stream;
+				stream.zalloc = Z_NULL;
+				stream.zfree = Z_NULL;
+				stream.opaque = Z_NULL;
+				stream.avail_in = 0;
+				stream.next_in = Z_NULL;
+				int ret = inflateInit2(&stream, 16 + MAX_WBITS);
+				if (ret != Z_OK) {
+					std::cerr << "Failed to initialize zlib: " << ret << std::endl;
+					return deflated_data;
+				}
+
+
+				char buffer[1000];
+				// 解压gzip格式数据
+				stream.next_in = (Bytef*)gzip_data;
+				stream.avail_in = (uInt)gzip_data_len;
+				do {
+					stream.next_out = (Bytef*)buffer;
+					stream.avail_out = sizeof(buffer);
+					ret = inflate(&stream, Z_NO_FLUSH);
+					if (ret < 0) {
+						std::cerr << "Failed to unzip gzip data: " << ret << std::endl;
+						inflateEnd(&stream);
+						return deflated_data;
+					}
+					deflated_data += std::string(buffer, sizeof(buffer) - stream.avail_out);
+					// std::cout.write(buffer, sizeof(buffer) - stream.avail_out);
+				} while (stream.avail_out == 0);
+
+				// 关闭zlib解压器
+				inflateEnd(&stream);
+
+				return deflated_data;
+			}
+		}
+		#endif // HAS_ZLIB
+
 		using namespace boost::variant2;
 		using http_stream  = beast::tcp_stream;
 		using https_stream = beast::ssl_stream<http_stream>;
@@ -116,6 +171,9 @@ namespace httpc
 				http::request<http::string_body> req{ option.verb, path, 11 };
 				req.set(http::field::host, url.host());
 				req.set(http::field::user_agent, "curl/7.82.0");
+				#ifdef HAS_ZLIB
+				req.set(http::field::accept_encoding, "gzip, deflate");
+				#endif
 				// req.set(http::field::connection, "close");
 				for (auto& header : option.headers)
 				{
@@ -162,6 +220,41 @@ namespace httpc
 
 				ret.code		 = static_cast<unsigned>(res.result());
 				ret.content_type = ret[http::field::content_type];
+#ifdef HAS_ZLIB
+				if (ret[http::field::content_encoding] == "gzip")
+				{
+					beast::string_view compressd_body = static_cast<http::response<http::string_body>&>(ret).body();
+					static_cast<http::response<http::string_body>&>(ret).body() = gzip::gunzip(compressd_body.data(), compressd_body.length());
+				}
+				else if (ret[http::field::content_encoding] == "deflate")
+				{
+					beast::string_view compressd_body = static_cast<http::response<http::string_body>&>(ret).body();
+
+					std::string dest;
+					dest.resize(2048);
+
+					uLongf destLen = dest.size();
+					uLong sourceLen = compressd_body.size();
+					int zlib_ret = Z_OK;
+
+					while(Z_MEM_ERROR == ( zlib_ret = uncompress2(reinterpret_cast<Bytef*>(dest.data()), &destLen, reinterpret_cast<const Bytef*>(compressd_body.data()), &sourceLen)))
+					{
+						dest.resize(dest.size()*2);
+						destLen = dest.size();
+					}
+					if (zlib_ret == Z_DATA_ERROR)
+					{
+						ret.content_type = ret[http::field::content_type];
+						ret.body		 = static_cast<http::response<http::string_body>&>(ret).body();
+						break;
+					}
+					if (zlib_ret == Z_OK)
+					{
+						dest.resize(destLen);
+						static_cast<http::response<http::string_body>&>(ret).body() = dest;
+					}
+				}
+#endif // HAS_ZLIB
 				ret.body		 = static_cast<http::response<http::string_body>&>(ret).body();
 
 				if (is_secure)
